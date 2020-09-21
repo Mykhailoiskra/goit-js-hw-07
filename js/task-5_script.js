eb35602557ced35190143c304869d86c1f68d837
Mykhailo Iskra <iskramichel@gmail.com>
2020-09-21 22:19:17 +0300

    Решает задачу через деструктуризацию e.target

--- js/task-5_script.js
+++ js/task-5_script.js
@@ -1,11 +1,16 @@
 const inputEl = document.querySelector("#name-input");
 const nameEl = document.querySelector("#name-output");
 
-function onInputChange() {
-  event.currentTarget.value !== ""
-    ? (nameEl.textContent = event.currentTarget.value)
-    : (nameEl.textContent = "незнакомец");
-  console.dir(event.currentTarget);
+// function onInputChange() {
+//   event.currentTarget.value !== ""
+//     ? (nameEl.textContent = event.currentTarget.value)
+//     : (nameEl.textContent = "незнакомец");
+//   console.dir(event.currentTarget);
+// }
+function onInputChange(e) {
+  const { value } = e.target;
+  nameEl.textContent = value !== "" ? value : "незнакомец";
+  console.dir(e.target);
 }
 
 inputEl.addEventListener("input", onInputChange);

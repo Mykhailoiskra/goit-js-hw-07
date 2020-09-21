f4ab5fbdf525643c47548bd89a1a5e4c5cf08559
Mykhailo Iskra <iskramichel@gmail.com>
2020-09-21 23:52:35 +0300

    Готовый метод создать

---
+++ js/task-8_script.js
@@ -0,0 +1,30 @@
+const refs = {
+  counter: document.querySelector("#controls input"),
+  createBtn: document.querySelector("#controls button[data-action='render']"),
+  destroyBtn: document.querySelector("#controls button[data-action='destroy']"),
+  boxesContainer: document.querySelector("#boxes"),
+};
+
+function createBoxes() {
+  for (let i = 0; i < Number(refs.counter.value); i += 1) {
+    let boxes = [];
+    const box = document.createElement("div");
+    box.style.width = `${30 + 10 * i}px`;
+    box.style.height = `${30 + 10 * i}px`;
+    box.style.backgroundColor = `rgb(${Math.floor(
+      Math.random() * (255 - 0) + 0
+    )}, ${Math.floor(Math.random() * (255 - 0) + 0)}, ${Math.floor(
+      Math.random() * (255 - 0) + 0
+    )})`;
+    boxes.push(box);
+    refs.boxesContainer.append(...boxes);
+  }
+}
+
+function destroyBoxes() {
+  const removeEl = refs.boxesContainer.querySelectorAll("div");
+
+  refs.boxesContainer.removeChild(...removeEl);
+}
+refs.createBtn.addEventListener("click", createBoxes);
+refs.destroyBtn.addEventListener("click", destroyBoxes);
